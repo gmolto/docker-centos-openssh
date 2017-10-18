8d15cc8dd8d6c8abea6c1225e37277f353d8a056
Germán Moltó <gmolto@dsic.upv.es>
2017-10-18 15:21:36 +0200

    Modified for generic non-priviled user account

--- start.sh
+++ start.sh
@@ -6,15 +6,15 @@
 test -f /etc/ssh/ssh_host_ecdsa_key || /usr/bin/ssh-keygen -q -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -C '' -N ''
 test -f /etc/ssh/ssh_host_rsa_key || /usr/bin/ssh-keygen -q -t rsa -f /etc/ssh/ssh_host_rsa_key -C '' -N ''
 test -f /etc/ssh/ssh_host_ed25519_key || /usr/bin/ssh-keygen -q -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -C '' -N ''
-test -f /root/.ssh/id_rsa || /usr/bin/ssh-keygen -t rsa -f /root/.ssh/id_rsa -N ''
-test -f /root/.ssh/id_rsa.pub || ssh-keygen -y -t rsa -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
-test -f /root/.ssh/authorized_keys || /usr/bin/cp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys
+test -f ~/.ssh/id_rsa || /usr/bin/ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ''
+test -f ~/.ssh/id_rsa.pub || ssh-keygen -y -t rsa -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
+test -f ~/.ssh/authorized_keys || /usr/bin/cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys
 
 # Change the owner.
-chown -R root:root /root/.ssh
+chown -R $USER:$USER ~/.ssh
 
 # Show the private key.
-/usr/bin/cat /root/.ssh/id_rsa
+/usr/bin/cat ~/.ssh/id_rsa
 /usr/bin/echo ""
 /usr/bin/echo "Please save the printed private RSA key and login using:"
 /usr/bin/echo "\"ssh -i \${savedkey} root@\${ipaddress}\""
